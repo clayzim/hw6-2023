bd2f270ed5a9630e3cc69e3a6bc865c9baad8ebd
clayzim <clayzim@umich.edu>
2023-11-09 20:32:37 -0500

    Implemented video speedup and slowdown

--- js/video.js
+++ js/video.js
@@ -14,7 +14,6 @@ window.addEventListener("load", function() {
 // });
 
 document.querySelector("#play").addEventListener("click", function() {
-	console.log(volumeSlider)
 	video.volume = volumeSlider.value * 0.01;
 	video.play();
 })
@@ -23,3 +22,30 @@ document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
 })
 
+// Utility function to update video playbackRate
+function updateSpeed(video, percentage, slower) {
+	let scaler = (100 - percentage) / 100;
+	// Slow down by percentage
+	if (slower) {
+		video.playbackRate *= scaler;
+	}
+	// Speed up by percentage
+	else {
+		video.playbackRate /= scaler;
+	}
+	// Reset playback rate to 100% when within margin of error
+	if (video.playbackRate > 0.99 && video.playbackRate < 1.01) {
+		video.playbackRate = 1.0;
+	}
+	console.log(`Speed is now ${video.playbackRate * 100}%`);
+}
+
+// Slow video down by 10%
+document.querySelector("#slower").addEventListener("click", function() {
+	updateSpeed(video, 10, true)
+})
+
+// Speed video up by 10%
+document.querySelector("#faster").addEventListener("click", function() {
+	updateSpeed(video, 10, false)
+})
