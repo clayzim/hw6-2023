22ff798cc448daf6aed3933c579e3524c7defe07
clayzim <clayzim@umich.edu>
2023-11-09 20:05:05 -0500

    Initialized variables referencing elements in DOM

--- js/video.js
+++ js/video.js
@@ -1,8 +1,12 @@
-var video;
+let video;
+let volumeSlider;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-
+	console.log("Good job opening the window");
+	video = document.querySelector("#player1");
+	volumeSlider = document.querySelector("#slider");
+	video.autoplay = false;
+	video.loop = false;
 });
 
 // document.querySelector("#play").addEventListener("click", function() {
